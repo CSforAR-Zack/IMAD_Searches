9fd6502ab7576880de8b1ff7a2e8806d9b034cf5
Zackary <zspink@nea.k12.ar.us>
2022-06-05 19:40:54 -0500

    Intial

---
+++ IMAD_Searches/IMAD_Searches/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace IMAD_Searches;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ IMAD_Searches/IMAD_Searches/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace IMAD_Searches;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
